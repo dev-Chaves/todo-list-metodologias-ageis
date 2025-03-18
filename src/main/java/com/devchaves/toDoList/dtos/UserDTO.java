d9d86cc51f32583860174a80b55776e135e08b51
chaves <chavesprogrammer@gmail.com>
2025-03-18 16:33:50 -0300

    removing lombok e add getter/setter/constructor

--- src/main/java/com/devchaves/toDoList/dtos/UserDTO.java
+++ src/main/java/com/devchaves/toDoList/dtos/UserDTO.java
@@ -1,14 +1,40 @@
 package com.devchaves.toDoList.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserDTO {
     private String name;
     private String email;
     private String password;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
