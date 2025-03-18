73b68077413f28eb0bd0455589321d01e1110df7
chaves <chavesprogrammer@gmail.com>
2025-03-18 16:34:35 -0300

    removing lombok add  getter/setter/constructor

--- src/main/java/com/devchaves/toDoList/entitys/TaskEntity.java
+++ src/main/java/com/devchaves/toDoList/entitys/TaskEntity.java
@@ -3,9 +3,6 @@ package com.devchaves.toDoList.entitys;
 import com.devchaves.toDoList.entitys.wrappers.PriorityWrapper;
 import com.devchaves.toDoList.entitys.wrappers.StatusWrapper;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -13,9 +10,6 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tb_tasks")
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 public class TaskEntity implements Serializable {
 
     @Id
@@ -72,6 +66,94 @@ public class TaskEntity implements Serializable {
         return PriorityWrapper.Priority.fromString(this.priority);
     }
 
+    public TaskEntity() {
+
+    }
+
+    public TaskEntity(Long taskId, String title, String text, String description, String status, String priority, LocalDate dueDate, LocalDateTime createdAt, LocalDateTime updateAt, UsersEntity userId) {
+        this.taskId = taskId;
+        this.title = title;
+        this.text = text;
+        this.description = description;
+        this.status = status;
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.createdAt = createdAt;
+        this.updateAt = updateAt;
+        this.userId = userId;
+    }
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public UsersEntity getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UsersEntity userId) {
+        this.userId = userId;
+    }
 }
 
 
